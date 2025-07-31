088706767319d6dc73eb17eacf6cd137e979158e
raspi_voice <158295465+CodeNube737@users.noreply.github.com>
2025-07-31 12:00:42 -0700

    Update motor_speed.ino
    back to driver mode

--- motor_speed.ino
+++ motor_speed.ino
@@ -1,26 +1,27 @@
 /*   
  *   Basic example code for controlling a stepper with the AccelStepper library
  *   Revised 2025/jul/29, by Mikhail R, to allow bi-directional output
- *   Revised 2025/jul/30, by Mikhail R, for bipolar driver, and using PPR to set max rotation speed at 1Hz
+ *   Revised 2025/jul/30, by Mikhail R, for bipolar driver, and using PPR to set max rotation speed at 1rev/s
+ *   Revised 2025/jul/31, by Mikhail R, returning to DRIVER mode, cuz bipolar/4wire setup is for no driver
  *      
  *   by Dejan, https://howtomechatronics.com
  *//////////////////////////////////////////////////////////////////////////////
 
 #include <AccelStepper.h>
 // pins
-#define PUL_PIN_pos 8
-#define PUL_PIN_neg 9
-#define DIR_PIN_pos 10
-#define DIR_PIN_neg 11
+#define PUL_PIN 8
+#define DIR_PIN 10
 #define ORDER_PIN A6 
 // numeric constants for fine-tuning
 #define MIDPOINT 3*1024/5 // 1-3V CW, 3-5V CCW
 #define PPR 400 // Steps per revolution (e.g. 200 for full-step, 400 for half-step)
+#define MIN_STEP 1000 // in µs, eg. 1000us = a 2ms period, or 500Hz MaxSpeed
 // Construct stepper motor
-AccelStepper stepper1(AccelStepper::FULL4WIRE, PUL_PIN_pos, PUL_PIN_neg, DIR_PIN_pos, DIR_PIN_neg); 
+AccelStepper stepper1(AccelStepper::DRIVER, PUL_PIN, DIR_PIN); 
 
 void setup() {
-  stepper1.setMaxSpeed(PPR); // Set maximum speed value for the stepper
+  stepper1.setMaxSpeed(PPR); // Sets maximum rotation speed at 1rev/s
+  stepper1.setMinPulseWidth(MIN_STEP); // otherwise step size is way too small
 }
 
 void loop() {
