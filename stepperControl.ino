da8baaef7d482333ac5ef78b2dacbcdef852b2c9
raspi_voice <158295465+CodeNube737@users.noreply.github.com>
2025-08-05 16:06:06 -0700

    Update stepperControl.ino

--- stepperControl.ino
+++ stepperControl.ino
@@ -1,7 +1,8 @@
 // stepperControl.ino
 /* By Mikhail R, 2025/aug/1
  *    Runs a stepper motor through a typical driver with DIR, PUL, and EN inputs
- * Revised By MR 2025/aug/5, made serial better, and fixed improper CW/CW set. (may take a few tries).
+ * Revised By MR 2025/aug/5, made serial better, and fixed improper CW/CW set. 
+ *    Also, constrained input to prevent URV/LRV failure.
  *    note: best debug is with serial on, or serial off and 2sec/div 'scope on RESPONSE_PIN
 */
 ////////////////////////////////////////////////////////////////////////
@@ -13,27 +14,27 @@
 #define ORDER_PIN A6
 #define RESPONSE_PIN A7
 // numeric constants for fine-tuning
-#define PPR 400         // created because Leadshine ctrllr sets ppr. 200 = full, 400 = 1/2... etc. Adjust MIN_PULSE with PPR.
-#define MIN_PULSE 1250  // in µs, eg. 1250us = a 2.5ms period, or 400Hz base-speed, at 400 ppr, that's 1rev/sec. Adjust MIN_PULSE with PPR.
-#define SPEED 90        // percentage of RDNY-speed
-#define ACCELERATION 1  // for CUSHION, must be positive. e.g. 5*30=150µs gained from acceleration
-#define PRECISION 10    // as small an integer as possible but can still read feedback (response)
-#define DEADBAND 50     // should be at least enough to cover noise, for exiting IDLE. must be larger at smaller PPR.
-#define CUSHION 5       // as big as desired, speed-up/slow-down before leaving/entering IDLE
-#define RDNY 10         // seconds per revolution (anticipated speed)
+#define PPR 400          // created because Leadshine ctrllr sets ppr. 200 = full, 400 = 1/2... etc. Adjust MIN_PULSE with PPR.
+#define MIN_PULSE 1250   // in µs, eg. 1250us = a 2.5ms period, or 400Hz base-speed, at 400 ppr, that's 1rev/sec. Adjust MIN_PULSE with PPR.
+#define SPEED 90         // percentage of RDNY-speed
+#define ACCELERATION 10  // for CUSHION, must be positive. e.g. 5*30=150µs gained from acceleration
+#define PRECISION 5      // as small an integer as possible but can still read feedback (response)
+#define DEADBAND 32      // should be at least enough to cover noise, for exiting IDLE. must be larger at smaller PPR.
+#define CUSHION 100      // as big as desired, speed-up/slow-down before leaving/entering IDLE
+#define RDNY 10          // seconds per revolution (anticipated speed)
 // global variables
 enum State { IDLE,
              CW,
              CCW };
 State motorState = IDLE;
-bool startCushion = false;  // a sort-of sub state flag
-bool endCushion = false;    // a sort-of sub state flag
-bool serialMode = false; // disables motor's EN pin
+bool startCushion = false;                                               // a sort-of sub state flag
+bool endCushion = false;                                                 // a sort-of sub state flag
+bool serialMode = false;                                                 // disables motor's EN pin
 const int baseDelay = (MIN_PULSE * 2 * 200 / PPR * 100 / SPEED * RDNY);  // see notes
-int stepDelay = baseDelay;                                         // starting value
-int orderInput, responseInput, differential, startPos, endPos;     // should never exceed ±1023, or ±5V
-//const int MINinput = 204;                                          // 1 Volt (4mA)... currently unused
-//const int MAXinput = 1023;                                         // 5 Volts (20mA)
+int stepDelay = baseDelay;                                               // starting value
+int orderInput, responseInput, differential, startPos, endPos;           // should never exceed ±1023, or ±5V
+const int MINinput = 204 + PRECISION;                                    // 1 Volt (4mA)
+const int MAXinput = 1023 - 67 - PRECISION;                              // 5 Volts-ish (20mA)... don't ask about the 67, that's from experiment
 //int counter = 0; // may help for future iterations?
 
 // helper functions
@@ -49,8 +50,8 @@ void runStepper(int stepDelay) {
 }
 
 void setCushion() {  // set flags (sub-state)
-  //startCushion = abs(responseInput - startPos) <= CUSHION;
-  //endCushion = abs(responseInput - endPos) <= CUSHION;
+  startCushion = abs(responseInput - startPos) <= CUSHION;
+  endCushion = abs(responseInput - endPos) <= CUSHION;
   if (motorState == IDLE) {  // IDLE state should turn-off all cushion/accel flags
     startCushion = false;
     endCushion = false;
@@ -58,11 +59,13 @@ void setCushion() {  // set flags (sub-state)
   return;
 }
 
-//int constrainInput(int value) { return constrain(value, MINinput, MAXinput); } // ... currently unused
+int constrainInput(int value) {
+  return constrain(value, MINinput, MAXinput);
+}
 
 void updateInputs() {  // updateInputs() and subtract to get int differential
-  orderInput = analogRead(ORDER_PIN);
-  responseInput = analogRead(RESPONSE_PIN);
+  orderInput = constrainInput(analogRead(ORDER_PIN));
+  responseInput = constrainInput(analogRead(RESPONSE_PIN));
   differential = orderInput - responseInput;
   // float orderVoltage = orderInput*5.0/ 1023.0;
   // float responseVoltage = responseInput*5.0 /1023.0;
@@ -105,7 +108,7 @@ void updateOutputs() {  // updateOutputs() based on state and differential. Most
     digitalWrite(EN_PIN, HIGH);  // Disable driver
     stepDelay = baseDelay;
   } else {
-    digitalWrite(EN_PIN, LOW);  // Enable driver
+    digitalWrite(EN_PIN, LOW);                              // Enable driver
     digitalWrite(DIR_PIN, motorState == CCW ? HIGH : LOW);  // Apply direction (may be reversed)
     // set speed delay of a cycle
     if (startCushion)
